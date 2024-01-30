88e969e2362571900241040d6dec62a3f7f7d2a9
SheaikhNazib <sheaikh.rahman@northsouth.edu>
2024-01-31 03:16:50 +0600

    button clear and review decoration done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Cart.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-const Cart = ({cart}) => {
-    
+const Cart = ({ cart }) => {
+
     let totalPrice = 0;
     let totalShipping = 0;
     let quantity = 0;
-    for(const product of cart){
-        product.quantity = product.quantity||1;
-        totalPrice+= product.price * product.quantity;
-        totalShipping+=product.shipping
-        quantity+=product.quantity;
+    for (const product of cart) {
+        product.quantity = product.quantity || 1;
+        totalPrice += product.price * product.quantity;
+        totalShipping += product.shipping
+        quantity += product.quantity;
     }
 
-    const tax = totalPrice*0.07;
+    const tax = totalPrice * 0.07;
 
-    const grandTotal = totalPrice+tax;
+    const grandTotal = totalPrice + tax;
 
     return (
         <div className='cart'>
@@ -25,6 +27,14 @@ const Cart = ({cart}) => {
             <p>Total Shipping charge:${totalShipping}</p>
             <p>Tax:${tax.toFixed(2)}</p>
             <p>Grand total:${grandTotal.toFixed(2)}</p>
+            <button className='btn-clear-cart'>
+                Clear Cart
+                <FontAwesomeIcon className='clear-cart-icon' icon={faTrashAlt} />
+            </button>
+            <button className='btn-review-order'>
+                <span>Review Order</span>
+                <FontAwesomeIcon className='review-order-icon' icon={faArrowRight} />
+            </button>
 
         </div>
     );
